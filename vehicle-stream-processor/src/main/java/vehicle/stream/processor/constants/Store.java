7ea7eb0dd16c2b8ec00dfd1e5b76112fd406f6fe
Harjit Brar <workandresearchbrar@gmail.com>
2025-08-15 16:24:08 +0800

    feat: (processor), add constant for state stores

---
+++ vehicle-stream-processor/src/main/java/vehicle/stream/processor/constants/Store.java
@@ -0,0 +1,6 @@
+package vehicle.stream.processor.constants;
+
+public interface Store {
+    String VEHICLE_SPEED_STORE = "vehicle-speed-store";
+    String VEHICLE_IDLE_STORE = "vehicle-idle-store";
+}
