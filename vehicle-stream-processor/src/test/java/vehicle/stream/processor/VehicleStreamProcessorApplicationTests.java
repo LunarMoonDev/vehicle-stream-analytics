3da89c5248afc7be9a7501ecd1d516a3e2b0c5f1
Harjit Brar <workandresearchbrar@gmail.com>
2025-08-15 16:36:49 +0800

    chore: (processor), add .git files plus some generic test

---
+++ vehicle-stream-processor/src/test/java/vehicle/stream/processor/VehicleStreamProcessorApplicationTests.java
@@ -0,0 +1,13 @@
+package vehicle.stream.processor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VehicleStreamProcessorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
