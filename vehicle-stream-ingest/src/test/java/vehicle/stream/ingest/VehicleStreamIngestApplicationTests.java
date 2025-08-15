f3da62c659219897b4a665e0173adf98682da543
Harjit Brar <workandresearchbrar@gmail.com>
2025-08-15 16:17:32 +0800

    chore: (ingest), add generic test class and .git files

---
+++ vehicle-stream-ingest/src/test/java/vehicle/stream/ingest/VehicleStreamIngestApplicationTests.java
@@ -0,0 +1,13 @@
+package vehicle.stream.ingest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VehicleStreamIngestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
