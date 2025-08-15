17945d640704c5e0c5d7311ab2143b669fc10eb7
Harjit Brar <workandresearchbrar@gmail.com>
2025-08-15 16:15:41 +0800

    feat: (ingest), add util for string and logging

---
+++ vehicle-stream-ingest/src/main/java/vehicle/stream/ingest/utils/StringUtil.java
@@ -0,0 +1,16 @@
+package vehicle.stream.ingest.utils;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class StringUtil {
+
+    public String mask(String given) {
+        if (given == null || given.length() <= 2) {
+            return given;
+        }
+
+        int maskLength = given.length() - 2;
+        return "*".repeat(maskLength) + given.substring(maskLength);
+    }
+}
