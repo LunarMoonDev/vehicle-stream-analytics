6cfab3e61d4bdcf6c29deea55b775f7ca022f476
Harjit Brar <workandresearchbrar@gmail.com>
2025-08-19 13:27:41 +0800

    feat: (dbt), add postgres server

---
+++ vehicle-stream-dbt/scripts/create-database.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+# TODO: fix this script to create the databases
+set -e
+# psql -U sample -d postgres -c "create database batch_prefect_db"
+# psql -U sample -d postgres -c "create database airflow"
